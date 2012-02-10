916c86901597daa7228c93669d866c17031cdb46
Christopher de Beer <christopherdebeer@gmail.com>
2012-02-10 15:59:25 +0000

    added jdom to test json

--- test/jason.test.js
+++ test/jason.test.js
@@ -1,6 +1,7 @@
 var jQuery = require("jquery");
 var Jason = require("../src/Jason.js");
 var assert = require("assert");
+var jsdom = require("jsdom");
 
 var test = ["p.item",{style: 'display:block;', class: 'anotherClass'},
 		["span",{id: "innerSPAN"}, "text of the inner span"],
@@ -10,9 +11,14 @@ var test = ["p.item",{style: 'display:block;', class: 'anotherClass'},
 		["a", {href: "/"}, "link"]
 	];
 
+var jsdom  = require("jsdom"),
+    window = jsdom.jsdom().createWindow();
+
+jsdom.jQueryify(window, 'http://code.jquery.com/jquery-1.7.min.js' , function() {
+  assert.equal("<a href=\"#\" >link</a>", Jason.toHtml([["a",{href:"#"},"link"]]));
+});
 
 
-assert.equal("<a href=\"#\" >link</a>", Jason.toHtml([["a",{href:"#"},"link"]]));
 
 // console.log("toHtml() 2 (container): ", Jason.toHtml(test, {format: "obj"}));
 // console.log("toJason() 1: ", Jason.toJason(Jason.toHtml(test, {format: "html"})));
